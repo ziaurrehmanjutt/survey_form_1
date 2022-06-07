d437cffabf8a28e8919e80b4c5442b67f4f5ffd3
adnan-syslab <adnan@syslabtechnologies.com>
2022-06-07 23:55:30 +0500

    dani

--- application/views/surveys/forms/form17.php
+++ application/views/surveys/forms/form17.php
@@ -15,122 +15,122 @@
   <form id="form1" method="POST">
     <div role="main" class="main py-5">
       <div class="container" id="part-one">
-        <section class="question bg-light border p-3 rounded-lg shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">189</strong>
+        <section class="question bg-light border p-3 rounded-lg shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">49</strong>
           <div class="d-block overflow-hidden">
             <h6 class="h6 mt-2">How high was your concentration during the interview</h6>
             <div class="options pt-2">
-              <div class="form-check form-check-inline">
-                <input <?= $data->concentration_during_interview == 'Very low' ? 'checked' : '' ?> class="form-check-input" value="Very low" type="radio" name="concentration_during_interview" />
-                <label class="form-check-label">Very low</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->concentration_during_interview == 'Very low' ? 'checked' : '' ?> class="form-check-input" value="Very low" type="radio" name="concentration_during_interview" />
+                <label for="q11_6" class="form-check-label">Very low</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->concentration_during_interview == 'Rather low' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather low" name="concentration_during_interview" />
-                <label class="form-check-label">Rather low</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->concentration_during_interview == 'Rather low' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather low" name="concentration_during_interview" />
+                <label for="q11_6" class="form-check-label">Rather low</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->concentration_during_interview == 'Partially' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Partially" name="concentration_during_interview" />
-                <label class="form-check-label">Partially</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->concentration_during_interview == 'Partially' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Partially" name="concentration_during_interview" />
+                <label for="q11_6" class="form-check-label">Partially</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->concentration_during_interview == 'Rather High' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather High" name="concentration_during_interview" />
-                <label class="form-check-label">Rather High</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->concentration_during_interview == 'Rather High' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather High" name="concentration_during_interview" />
+                <label for="q11_6" class="form-check-label">Rather High</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->concentration_during_interview == 'Very High' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Very High" name="concentration_during_interview" />
-                <label class="form-check-label">Very High</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->concentration_during_interview == 'Very High' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Very High" name="concentration_during_interview" />
+                <label for="q11_6" class="form-check-label">Very High</label>
               </div>
             </div>
           </div>
           <div class="clearfix"></div>
         </section>
-        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">190</strong>
+        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">50</strong>
           <div class="d-block overflow-hidden">
             <h6 class="h6 mt-2">With what degree of honesty did you answer our questions</h6>
             <div class="options pt-2">
-              <div class="form-check form-check-inline">
-                <input <?= $data->degree_of_honesty == 'Not honest at all' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Not honest at all" name="degree_of_honesty" />
-                <label class="form-check-label">Not honest at all</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->degree_of_honesty == 'Not honest at all' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Not honest at all" name="degree_of_honesty" />
+                <label for="q11_6" class="form-check-label">Not honest at all</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->degree_of_honesty == 'Rather not honest' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather not honest" name="degree_of_honesty" />
-                <label class="form-check-label">Rather not honest</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->degree_of_honesty == 'Rather not honest' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather not honest" name="degree_of_honesty" />
+                <label for="q11_6" class="form-check-label">Rather not honest</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->degree_of_honesty == 'Partially' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Partially" name="degree_of_honesty" />
-                <label class="form-check-label">Partially</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->degree_of_honesty == 'Partially' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Partially" name="degree_of_honesty" />
+                <label for="q11_6" class="form-check-label">Partially</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->degree_of_honesty == 'Rather honest' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather honest" name="degree_of_honesty" />
-                <label class="form-check-label">Rather honest</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->degree_of_honesty == 'Rather honest' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather honest" name="degree_of_honesty" />
+                <label for="q11_6" class="form-check-label">Rather honest</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->degree_of_honesty == 'Very honest' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Very honest" name="degree_of_honesty" />
-                <label class="form-check-label">Very honest</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->degree_of_honesty == 'Very honest' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Very honest" name="degree_of_honesty" />
+                <label for="q11_6" class="form-check-label">Very honest</label>
               </div>
             </div>
           </div>
           <div class="clearfix"></div>
         </section>
 
-        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">191</strong>
+        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">51</strong>
           <div class="d-block overflow-hidden">
             <h6 class="h6 mt-2">Wie stark vertrauen Sie unseren Maßnahmen zur Anonymität und zum Persönlichkeitsschutz der Teilnehmenden in dieser Umfrage</h6>
             <div class="options pt-2">
-              <div class="form-check form-check-inline">
-                <input <?= $data->personlichkeitsschutz == 'Not at all' ? 'checked' : '' ?> class="form-check-input" value="Not at all" type="radio" name="personlichkeitsschutz" />
-                <label class="form-check-label">Not at all</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->personlichkeitsschutz == 'Not at all' ? 'checked' : '' ?> class="form-check-input" value="Not at all" type="radio" name="personlichkeitsschutz" />
+                <label for="q11_6" class="form-check-label">Not at all</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->personlichkeitsschutz == 'Rather not' ? 'checked' : '' ?> class="form-check-input" value="Rather not" type="radio" name="personlichkeitsschutz" />
-                <label class="form-check-label">Rather not</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->personlichkeitsschutz == 'Rather not' ? 'checked' : '' ?> class="form-check-input" value="Rather not" type="radio" name="personlichkeitsschutz" />
+                <label for="q11_6" class="form-check-label">Rather not</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->personlichkeitsschutz == 'Partially' ? 'checked' : '' ?> class="form-check-input" value="Partially" type="radio" name="personlichkeitsschutz" />
-                <label class="form-check-label">Partially</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->personlichkeitsschutz == 'Partially' ? 'checked' : '' ?> class="form-check-input" value="Partially" type="radio" name="personlichkeitsschutz" />
+                <label for="q11_6" class="form-check-label">Partially</label>
 
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->personlichkeitsschutz == 'Rather Strong' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather Strong" name="personlichkeitsschutz" />
-                <label class="form-check-label">Rather Strong</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->personlichkeitsschutz == 'Rather Strong' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather Strong" name="personlichkeitsschutz" />
+                <label for="q11_6" class="form-check-label">Rather Strong</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->personlichkeitsschutz == 'Very Strong' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Very Strong" name="personlichkeitsschutz" />
-                <label class="form-check-label">Very Strong</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->personlichkeitsschutz == 'Very Strong' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Very Strong" name="personlichkeitsschutz" />
+                <label for="q11_6" class="form-check-label">Very Strong</label>
               </div>
             </div>
           </div>
           <div class="clearfix"></div>
         </section>
 
-        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">192</strong>
+        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">52</strong>
           <div class="d-block overflow-hidden">
             <h6 class="h6 mt-2">Denken Sie, dass Sie die besonderen Befragungsmethoden jeweils korrekt befolgt haben</h6>
             <div class="options pt-2">
-              <div class="form-check form-check-inline">
-                <input <?= $data->befragungsmethoden == 'Not at all' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Not at all" name="befragungsmethoden" />
-                <label class="form-check-label">Not at all</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->befragungsmethoden == 'Not at all' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Not at all" name="befragungsmethoden" />
+                <label for="q11_6" class="form-check-label">Not at all</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->befragungsmethoden == 'Rather not' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather not" name="befragungsmethoden" />
-                <label class="form-check-label">Rather not</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->befragungsmethoden == 'Rather not' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather not" name="befragungsmethoden" />
+                <label for="q11_6" class="form-check-label">Rather not</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->befragungsmethoden == 'Partially' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Partially" name="befragungsmethoden" />
-                <label class="form-check-label">Partially</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->befragungsmethoden == 'Partially' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Partially" name="befragungsmethoden" />
+                <label for="q11_6" class="form-check-label">Partially</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->befragungsmethoden == 'Rather Yes' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather Yes" name="befragungsmethoden" />
-                <label class="form-check-label">Rather Yes</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->befragungsmethoden == 'Rather Yes' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Rather Yes" name="befragungsmethoden" />
+                <label for="q11_6" class="form-check-label">Rather Yes</label>
               </div>
-              <div class="form-check form-check-inline">
-                <input <?= $data->befragungsmethoden == 'Yes definitely' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Yes definitely" name="befragungsmethoden" />
-                <label class="form-check-label">Yes definitely</label>
+              <div class="form-check">
+                <input id="q11_6" <?= $data->befragungsmethoden == 'Yes definitely' ? 'checked' : '' ?> class="form-check-input" type="radio" value="Yes definitely" name="befragungsmethoden" />
+                <label for="q11_6" class="form-check-label">Yes definitely</label>
               </div>
             </div>
           </div>
           <div class="clearfix"></div>
         </section>
-        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">193</strong>
+        <section class="question bg-light border p-3 rounded-lg mt-4 shadow"> <strong class="float-left bg-primary q-number text-center text-white rounded-circle mr-3">53</strong>
           <div class="d-block overflow-hidden">
             <h6 class="h6 mt-2">If you share your team name with us and more than two teammates have filled in the questionnaire, we will send you an analyzed exposé with your results that shows your results in comparison to other start-ups and possible rooms for improvement.</h6>
             <div class="options pt-2">
